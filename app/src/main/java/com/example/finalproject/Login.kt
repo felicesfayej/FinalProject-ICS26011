084f919df7d3b2ce2a91b9257002a64a92d1128e
felicesfayej <felicesfayejustin@gmail.com>
2023-11-22 07:03:26 +0800

    -4 XML files - No IDs yet

--- app/src/main/java/com/example/finalproject/Login.kt
+++ app/src/main/java/com/example/finalproject/Login.kt
@@ -1,18 +1,18 @@
 package com.example.finalproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
     }
 
-    fun toLogin (view: View){
-        val i = Intent(this,Login::class.java)
+    fun toRegister(view: View) {
+        val i = Intent(this, Register::class.java)
         startActivity(i)
     }
 }
--- app/src/main/java/com/example/finalproject/RegisterConfirm.kt
+++ app/src/main/java/com/example/finalproject/RegisterConfirm.kt
@@ -1,18 +1,19 @@
 package com.example.finalproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RegisterConfirm : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.register_confirm)
     }
 
-    fun toLogin (view: View){
-        val i = Intent(this,Login::class.java)
+    fun toLogin(view: View) {
+        val i = Intent(this, Login::class.java)
         startActivity(i)
     }
+
 }
